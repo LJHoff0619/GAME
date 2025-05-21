cd31268762d8b0ab5bae3db17f07027e4d3bd3bb
dongjunkimm <86935088+dongjunkimm@users.noreply.github.com>
2025-05-21 15:16:31 +0000

    try this code, might be broken, revert this asap

--- GAME/src/JB.java
+++ GAME/src/JB.java
@@ -17,97 +17,117 @@ public class JB extends JComponent implements Runnable, KeyListener
 	private BufferedImage JB_not_moving;
 	private BufferedImage JB_left;
 	private BufferedImage JB_right;
-	private int x_pos;
-	private int x_speed;
-	private int y_pos;
-	private int y_speed;
-	public JB() 
+	private BufferedImage x;
+
+	private boolean[] keys = new boolean[256];
+
+	public JB()
 	{
-		
-		//All initializations go here
-		x_pos = 10;
-		y_pos = 200;
-		x_speed = 5;
-		y_speed = 5;
-		addKeyListener(this);
-		setFocusable(true);
-		setFocusTraversalKeysEnabled(false);
-		try 
+
+
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		this.setFocusTraversalKeysEnabled(false);
+
+		try
 		{
-		    JB_down = ImageIO.read(new File("JB_down.jpg"));
-		} 
-		catch (IOException e) 
-		{
-		    e.printStackTrace();
+			JB_down = ImageIO.read(Test.class.getResourceAsStream("JB_down.jpg"));
 		}
-		
-		try 
+		catch(IOException | IllegalArgumentException e)
 		{
-		    JB_down = ImageIO.read(new File("JB_down.jpg"));
-		} 
-		catch (IOException e) 
-		{
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
+
+		try
+		{
+			JB_left = ImageIO.read(Test.class.getResourceAsStream("JB_left.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+
+
+		try
+		{
+			JB_up = ImageIO.read(Test.class.getResourceAsStream("JB_up.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			JB_right = ImageIO.read(Test.class.getResourceAsStream("JB_right.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+
 	}
+
+	public void up()
+	{
+		x = JB_up;
+	}
+	public void down()
+	{
+		x = JB_down;
+	}
+	public void left()
+	{
+		x = JB_left;
+	}
+	public void right()
+	{
+		x = JB_right;
+	}
+
+
 	public void startAnimation()
 	{
- 		Thread t = new Thread(this);
+		Thread t = new Thread(this);
 		t.start();
 	}
-	
+
 	public void paintComponent(Graphics g)
 	{
-		g.drawImage(JB_down, 100, 200, null);
+		g.drawImage(x, 100, 200, null);
+
 	}
-	public void keyPressed(KeyEvent e)
+	public void keyTyped(KeyEvent e) 
 	{
-		System.out.println(e.getKeyCode());
-		if(e.getKeyCode()==37)
-		{
-			System.out.println(e.getKeyCode());
-		}
-		else if(e.getKeyCode()==46)
-		{
-			y_speed*=-1;
-		}
+
+
+
 	}
-	public void keyReleased(KeyEvent e)
-	{
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+		System.out.println(e.getKeyChar());
 	}
-	public void keyTyped(KeyEvent e)
-	{
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
 	}
+
+
+
 	public void run()
 	{
-		while (true)
-		{
-			try
-			{
-				if (x_pos>=930)
-					x_speed=0;
-					y_speed=0;
-				if (x_pos <=0)
-					x_speed=0;
-					y_speed=0;
-				if (y_pos>=500)
-					y_speed=0;
-					x_speed=0;
-				if (y_pos <=0)
-					y_speed=0;
-					x_speed=0;
-				
-				x_pos+=x_speed;
-				y_pos+=y_speed;
-				repaint();
-				Thread.sleep(10);
-			}
-			catch (InterruptedException e)
-			{
-			
-			}
+		while (true) {
+			if (keys[KeyEvent.VK_LEFT]) left();
+			if (keys[KeyEvent.VK_UP]) up();
+			if (keys[KeyEvent.VK_DOWN]) down();
+			if (keys[KeyEvent.VK_RIGHT]) right();
+
+			repaint();
 		}
 	}
-	
-	
+
+
 }
