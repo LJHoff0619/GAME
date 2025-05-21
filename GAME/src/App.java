fe291bc32496cd3f889045e3cc1a65fe434e7aa5
dongjunkimm <86935088+dongjunkimm@users.noreply.github.com>
2025-05-21 11:45:46 -0400

    Update App.java

--- GAME/src/App.java
+++ GAME/src/App.java
@@ -2,29 +2,30 @@ import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 import javax.swing.JButton;
 
-public class App
+public class Test 
 {
+	
 
-	public static void main(String []args) throws IOException
+	public static void main(String []args) 
 	{
-		
 		JFrame frame = new JFrame();
 		final int FRAME_WIDTH = 1000;
 		final int FRAME_HEIGHT = 600;
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
 		JB jb= new JB();
 		frame.add(jb,BorderLayout.CENTER);
-		frame.setVisible(true); 
+		frame.setVisible(true);
 		jb.startAnimation();
-		
-		
+//		jb.up();
+//		jb.run();
 	
-	}
+	}	
 }
