73b2ff28158f03ebec600e3358a4deaef52d21ea
LJHoff0619 <ljhoff0619@gmail.com>
2025-06-02 13:00:38 -0400

    Update App.java

--- GAME/src/App.java
+++ GAME/src/App.java
@@ -1,31 +1,32 @@
 import javax.swing.JFrame;
-
 import java.awt.BorderLayout;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
-
 import javax.swing.JButton;
 
-public class Test 
+public class Test
 {
-	
 
-	public static void main(String []args) 
+	public static void main(String []args) throws IOException
 	{
+		
 		JFrame frame = new JFrame();
 		final int FRAME_WIDTH = 1000;
 		final int FRAME_HEIGHT = 600;
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		JB jb= new JB();
+		
+		final JB jb= new JB();
 		frame.add(jb,BorderLayout.CENTER);
-		frame.setVisible(true);
+		frame.setVisible(true); 
 		jb.startAnimation();
-//		jb.up();
-//		jb.run();
+		
+		final Arrows a= new Arrows();
+		frame.add(a,BorderLayout.EAST);
+		frame.setVisible(true);
+		a.startAnimation();
+		
+		
 	
-	}	
+	}
 }
