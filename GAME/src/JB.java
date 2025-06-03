e8f54e9fa6a05dc876fa5adfe1566a75720a4ec4
dongjunkimm <86935088+dongjunkimm@users.noreply.github.com>
2025-06-03 12:13:25 -0400

    Update JB.java

--- GAME/src/JB.java
+++ GAME/src/JB.java
@@ -17,14 +17,50 @@ public class JB extends JComponent implements Runnable, KeyListener
 	private BufferedImage JB_not_moving;
 	private BufferedImage JB_left;
 	private BufferedImage JB_right;
+	private BufferedImage AR;
+	private BufferedImage AL;
+	private BufferedImage AU;
+	private BufferedImage AD;
 	private BufferedImage x;
-	private Sound s;
+	private BufferedImage y;
+	private BufferedImage hoop;
+	private BufferedImage swoosh;
+	private BufferedImage good;
+	private BufferedImage ok;
+	private BufferedImage booty;
+	
+	private BufferedImage onethree;
+	
+	private sound s;
+	private int x_pos;
+	private int x_speed;
+	private int y_pos;
+	private int y_pos2;
+	private int y_pos3;
+	private int y_pos4;
 	private boolean[] keys = new boolean[256];
+	
+	
+	
+	private int score;
 
 	public JB()
 	{
+		
+		x_pos = 385;
+		y_pos = -100;
+		y_pos2= -200;
+		y_pos3= -300;
+		y_pos4= -400;
+		x_speed = 8;
+		
+		score = 0;
+		
+		s = new sound("C:\\Users\\27kimd01\\Downloads\\otj.wav");
 
-		s = new Sound("G:\\My Drive\\10th Grade\\APCSA\\0_GAME_0\\src\\otj.wav");
+		
+
+//		s = new Sound();
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
@@ -36,6 +72,64 @@ public class JB extends JComponent implements Runnable, KeyListener
 		{
 			e.printStackTrace();
 		}
+		
+		try
+		{
+			swoosh = ImageIO.read(Test.class.getResourceAsStream("swoosh.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			onethree = ImageIO.read(Test.class.getResourceAsStream("jb3-1.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			booty = ImageIO.read(Test.class.getResourceAsStream("booty.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		try
+		{
+			good = ImageIO.read(Test.class.getResourceAsStream("good.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		try
+		{
+			ok = ImageIO.read(Test.class.getResourceAsStream("ok.jpg"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		try
+		{
+			hoop = ImageIO.read(Test.class.getResourceAsStream("images__3_-removebg-preview (2).png"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
 
 		try
 		{
@@ -67,12 +161,55 @@ public class JB extends JComponent implements Runnable, KeyListener
 
 		try
 		{
+			AR = ImageIO.read(Test.class.getResourceAsStream("right.png"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			AL = ImageIO.read(Test.class.getResourceAsStream("left.png"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			AU = ImageIO.read(Test.class.getResourceAsStream("upp.png"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		try
+		{
+			AD = ImageIO.read(Test.class.getResourceAsStream("down.png"));
+		}
+		catch(IOException | IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}	
+		
+		
+		try
+		{
 			JB_right = ImageIO.read(Test.class.getResourceAsStream("JB_new_right.jpg"));
 		}
 		catch(IOException | IllegalArgumentException e)
 		{
 			e.printStackTrace();
 		}
+		
+		
+		
 
 	}
 
@@ -96,7 +233,31 @@ public class JB extends JComponent implements Runnable, KeyListener
 	{
 		x= JB_not_moving;
 	}
-
+	
+	
+	public void swoosh()
+	{
+		y = swoosh;
+	}
+	public void good()
+	{
+		y = good;
+	}
+	public void ok()
+	{
+		y = ok;
+	}
+	public void booty()
+	{
+		y = booty;
+	}
+	public void onethree()
+	{
+		y= onethree;
+	}
+	
+	
+	
 
 	public void startAnimation()
 	{
@@ -108,6 +269,18 @@ public class JB extends JComponent implements Runnable, KeyListener
 	public void paintComponent(Graphics g)
 	{
   		g.drawImage(x, 100, 200, null);
+  		
+  		g.drawImage(AU, 750, y_pos3, null);
+  		g.drawImage(AD, 600, y_pos4, null);
+  		g.drawImage(AL, 450, y_pos, null);
+  		g.drawImage(AR,900,y_pos2,null);
+  		
+  		g.drawImage(hoop, 400, 400, null);
+  		g.drawImage(hoop, 550, 400, null);
+  		g.drawImage(hoop, 700, 400, null);
+  		g.drawImage(hoop, 850, 400, null);
+  		
+  		g.drawImage(y, 100, 50,null);
 
 	}
 	public void keyTyped(KeyEvent e) 
@@ -118,13 +291,92 @@ public class JB extends JComponent implements Runnable, KeyListener
 	
 	public void keyPressed(KeyEvent e) 
 	{
-		System.out.println(e.getKeyCode());
-		if (e.getKeyCode() == 37) left();
-		if (e.getKeyCode() == 39) right();
-		if (e.getKeyCode() == 38) up();
-		if (e.getKeyCode() == 40) down();
 		
-	}
+		if (e.getKeyCode() == 37) 
+			{
+			left();
+				if((y_pos<= 450 && y_pos >425) || (y_pos< 375 && y_pos >= 350))
+				{
+					good();
+					
+					score+=2;
+					System.out.println("+2! score:" + score);
+					y_pos = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+					
+					
+				}
+				if(y_pos <= 425 && y_pos >= 375)
+				{
+					swoosh();
+					score+=3;
+					System.out.println("+3! score: " + score);
+					
+					y_pos = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+				}
+				
+			}
+		if (e.getKeyCode() == 39) 
+		{
+			right();
+			if((y_pos2<= 450 && y_pos2 >425) || (y_pos2< 375 && y_pos2 >= 350))
+			{
+				good();
+				score+=2;
+				System.out.println("+2! score:" + score);
+				y_pos2 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+				
+			}
+			if(y_pos2 <= 425 && y_pos2 >= 375)
+			{
+				swoosh();
+				score+=3;
+				System.out.println("+3! score: " + score);
+				y_pos2 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+			}
+		}
+		
+		if (e.getKeyCode() == 38) 
+			{
+			up();
+			if((y_pos3<= 450 && y_pos3 >425) || (y_pos3< 375 && y_pos3 >= 350))
+			{
+				good();
+				score+=2;
+				System.out.println("+2! score:" + score);
+				y_pos3 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+			
+				
+			}
+			if(y_pos3 <= 425 && y_pos3 >= 375)
+			{
+				swoosh();
+				score+=3;
+				System.out.println("+3! score: " + score);
+				y_pos3 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+			}
+			}
+		if (e.getKeyCode() == 40) 
+		{
+		down();
+		if((y_pos4<= 450 && y_pos4 >425) || (y_pos4< 375 && y_pos4 >= 350))
+		{
+			good();
+			score+=2;
+			System.out.println("+2! score:" + score);
+			y_pos4 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+			
+		}
+		if(y_pos4 <= 425 && y_pos4 >= 375)
+		{
+			swoosh();
+			score+=3;
+			System.out.println("+3! score: " + score);
+			y_pos4 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+		}
+		}
+		}
+		
+
 
 
 	public void keyReleased(KeyEvent e) 
@@ -146,11 +398,52 @@ public class JB extends JComponent implements Runnable, KeyListener
 	public void run()
 	{
 		s.makeSound();
+
 		while (true) {
 			try {
+				y_pos+=20;
+				y_pos2+=20;
+				y_pos3+=20;
+				y_pos4+=20;
+				if(y_pos > 600)
+				{
+					booty();
+					y_pos = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+					score-= 1;
+					System.out.println("-1! :" + score +"  " + y_pos);
+				}
+				
+				if(y_pos2 > 600)
+				{
+					booty();
+					y_pos2 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+					score-= 1;
+					System.out.println("-1! :" + score);
+				}
+				
+				if(y_pos3 > 600)
+				{
+					booty();
+					y_pos3 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+					score-= 1;
+					System.out.println("-1! :" + score);
+				}
+				
+				if(y_pos4 > 600)
+				{
+					booty();
+					y_pos4 = (int) ((-1 * (Math.random()*(1500-500)/100)))*100+-100;
+					score-= 1;
+					System.out.println("-1! :" + score);
+				}
+				
+				
+				
+				
 				
 				repaint();
 				Thread.sleep(20);
+				
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -159,3 +452,5 @@ public class JB extends JComponent implements Runnable, KeyListener
 
 
 }
+
+
