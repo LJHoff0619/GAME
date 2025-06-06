53bb5a76b5001eea0d7f8519c15ca63243885993
LJHoff0619 <ljhoff0619@gmail.com>
2025-06-05 23:16:29 -0400

    COMMENTS

--- GAME/src/App.java
+++ GAME/src/App.java
@@ -1,5 +1,11 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Frame;
 import java.awt.Graphics;
 import java.io.IOException;
 import javax.swing.JButton;
@@ -9,22 +15,18 @@ public class Test
 
 	public static void main(String []args) throws IOException
 	{
-		
+		//creates JFrame to put pictures on top of 	
 		JFrame frame = new JFrame();
-		final int FRAME_WIDTH = 1000;
-		final int FRAME_HEIGHT = 600;
-		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+		//when you close the window, it will terminate the code
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		//All the photos we have and setting the layout of the border to the center
 		final JB jb= new JB();
 		frame.add(jb,BorderLayout.CENTER);
+		//makes the Frame visible and starts running the code
 		frame.setVisible(true); 
 		jb.startAnimation();
 		
-		final Arrows a= new Arrows();
-		frame.add(a,BorderLayout.EAST);
-		frame.setVisible(true);
-		a.startAnimation();
 		
 		
 	
